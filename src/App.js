b65b3e742e96241a9ce5fb49c59b64ce25ea9cd0
dpesherin <pesherind@yandex.ru>
2025-01-02 18:02:00 +0300

    folder name fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import  "./App.css"
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Content from "./components/Content/Content";
-import PrivateRouter from "./components/services/PrivateRouter"
+import PrivateRouter from "./components/Services/PrivateRouter"
 //IMPORT PAGES
 import Home from "./pages/Home/Home";
 import Inventory from './pages/Inventory/Inventory';
 import User from './pages/Profile/User';
 import Login from './pages/Login/Login';
-import ConditionalSidebar from './components/services/ConditionalSidebar';
+import ConditionalSidebar from './components/Services/ConditionalSidebar';
 //END IMPORT
 
 function App() {
